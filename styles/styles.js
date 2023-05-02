c8cf3a810324d752b43dffbc2f457fe9d408ae36
Philip Munaawa <munaawap@kainos.com>
2023-05-02 16:35:14 +0100

    change background

--- styles/styles.js
+++ styles/styles.js
@@ -1,6 +1,6 @@
 export const overlay = {
   minHeight: "100vh",
-  backgroundColor: "rgba(0, 0, 0, 0.5)",
+  backgroundColor: "rgba(0, 0, 0, 0.3)",
   display: "flex",
   alignItems: "center",
   justifyContent: "center",
